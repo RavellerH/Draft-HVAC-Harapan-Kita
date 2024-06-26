49332029532ae427ce98d03b47cf7a3be2d0fc50
Farhan Budiman <106648310+RavellerH@users.noreply.github.com>
2024-06-26 15:55:14 +0700

    cleaned up v2

--- js/dosing.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
-        const dosing = document.getElementById('dosing');
-        dosing.classList.add('active');
-    }, 2);
-});
--- js/reservoir.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    setTimeout(function () {
-        const reservoir = document.getElementById('reservoir');
-        reservoir.classList.add('active');
-    }, 2);
-});
